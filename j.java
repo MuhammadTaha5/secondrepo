5642febd93563d4d1e1f87fa39899f80275fa348
MuhammadTaha5 <taha.saeed339@gmail.com>
2024-02-26 01:10:08 +0500

    second branch

--- j.java
+++ j.java
@@ -3,5 +3,6 @@ package secondrepo;
 public class j {
     public static void main(String[] args) {
         System.out.println("Second Repository");
+        System.out.println("this is branch 2");
     }
 }
