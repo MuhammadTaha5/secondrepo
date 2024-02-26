7960eb4fa93cf299af5c9daa4dfbe8cc83ade119
MuhammadTaha5 <taha.saeed339@gmail.com>
2024-02-26 15:05:08 +0500

    Sum of two numbers Program

--- j.java
+++ j.java
@@ -1,9 +1,17 @@
 package secondrepo;
-
+import java.util.Scanner;
 public class j {
     public static void main(String[] args) {
         System.out.println("Second Repository");
-        System.out.println("this is branch 2");
-        System.out.println("name");
+        Scanner input =new Scanner(System.in);
+        int num1, num2, sum;
+        System.out.print("Enter Number1: ");
+        num1=input.nextInt();
+        System.out.print("Enter Number2: ");
+        num2=input.nextInt();
+        sum=num1+num2;
+        
+        System.out.println("Sum of two Numbers: "+sum);
+        
     }
 }
